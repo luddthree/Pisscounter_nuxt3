bbd96e3cd6bffa19a2a746f9dfcba78e4cefdd87
luddthree <luddetv@gmail.com>
2023-09-13 15:42:20 +0200

    map unlogged added

--- components/footer.vue
+++ components/footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="mb-auto fixed bottom-2 w-full">
+    <!-- <div class="mb-auto fixed bottom-2 w-full">
         <div class="bg-gray-300 text-center">
             <p>&copy; 2023 PissCounter. All rights reserved.</p>
         </div>
-    </div>
+    </div> -->
 </template>
---
+++ pages/map.vue
@@ -0,0 +1,46 @@
+<template>
+    <body class="bg-gray-300">
+        <Header /><div>
+        
+        
+        <div class="text-center">
+<h1 class="text-4xl font-bold">PissMap</h1>
+<p>Show us some places you have pissed!</p>
+<br>
+</div>
+<div>
+<!-- <img src="/static/maplock.jpg" class="justify-center items-center snap-center self-center"> -->
+<div class="flex items-center justify-center">
+  <img src="/maplock.jpg" alt="maplock" class="object-cover md:h-full w-full">
+</div>
+
+
+
+        <!-- <h1 class="text-5xl text-red-600 font-bold text-center absolute">You have to sign in to access this page!</h1> -->
+
+
+        <br>
+</div>
+        
+<div class="text-center text-lg">
+<b><a href="/logon" class="text-blue-700 hover:text-blue-900">Sign in </a> to access this page!</b>
+</div>
+
+
+        <!-- <div class="flex justify-center">
+            <button type="submit" value="login" class="daddy bg-purple-500 hover:bg-purple-800 text-white font-bold py-3 px-6 rounded"><span class="text">Sign in</span></button><br>
+            </div><br> -->
+    
+            <div class="flex justify-center">
+                <p class="text-gray-700 text-sm">Or if you havent already</p>
+                </div><br>
+    
+   
+    
+    <div class="flex justify-center">    
+        
+        <button class="daddy bg-purple-500 hover:bg-purple-800 text-white font-bold py-3 px-6 rounded"><a href="/enternew" class="button-64"><span class="text">Sign Up</span></a></button>
+</div>
+    </div></body>
+    <br><br><br>
+</template>
