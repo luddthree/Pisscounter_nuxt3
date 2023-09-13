01d3235d95ac81c806c203d1c8c52d913029845c
luddthree <luddetv@gmail.com>
2023-09-13 15:20:41 +0200

    css file gone

--- components/header.vue
+++ components/header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav
-      class="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 dark:text-neutral-200 md:flex-wrap md:justify-start"
+      class="relative flex w-full items-center justify-between bg-white py-2 text-neutral-600 shadow-lg  md:flex-wrap md:justify-start"
       data-te-navbar-ref>
       <div class="flex w-full flex-wrap items-center justify-between px-3">
         <div class="flex items-center">
@@ -20,17 +20,17 @@
           class="bg-purple-500 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded right-2 absolute">Sign
           up</button>
         <div class="mb-4 lg:mb-0 lg:pr-2" data-te-nav-item-ref>
-          <a class="block transition duration-150 ease-in-out right-28 top-4 absolute font-bold hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+          <a class="block transition duration-150 ease-in-out right-28 top-4 absolute font-bold hover:text-neutral-900"
             href="/logon" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">Sign in</a>
         </div>
         <p class="right-44 absolute">|</p>
         <div class="mb-4 lg:mb-0 lg:pr-2 font-bold" data-te-nav-item-ref>
-          <a class="block transition duration-150 ease-in-out right-48 top-4 absolute hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+          <a class="block transition duration-150 ease-in-out right-48 top-4 absolute hover:text-neutral-900"
             href="/about" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">About</a>
         </div>
-        <p class="right-62 absolute">|</p>
+        <p class="right-64 absolute">|</p>
         <div class="mb-4 lg:mb-0 lg:pr-2 font-bold" data-te-nav-item-ref>
-          <a class="block transition duration-150 ease-in-out right-68 top-4 absolute hover:text-neutral-700 focus:text-neutral-700 disabled:text-black/30 dark:hover:text-white dark:focus:text-white lg:p-2 [&.active]:text-black/90"
+          <a class="block transition duration-150 ease-in-out right-72 top-4 absolute hover:text-neutral-900"
             href="/map" data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light">PissMap</a>
         </div>
       </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default {
-  
-  css: ['C:/Users/ludde/pisscounter/assets/css/main.css'],
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
-  }
-}
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
-
-
+export default defineNuxtConfig ({
+  devtools: { enabled: true },
+  modules : ["@nuxtjs/tailwindcss"]
+})
